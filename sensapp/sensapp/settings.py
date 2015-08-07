fcd9dec8bc83cc74c4e14ddbaba89cbbcce883fd
ukrishnank83 <ukrishnank83@gmail.com>
2015-08-07 15:22:38 +0530

    database also integrated

--- sensapp/sensapp/settings.py
+++ sensapp/sensapp/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'sensapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sens',
+        'USER': 'root',
+        'PASSWORD': 'krishnunni',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
